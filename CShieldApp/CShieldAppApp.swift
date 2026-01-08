af9be7e76bc1624a7bc76ea4accd8bdb8401f48f
levanthanhlong <thanhtdbn132001@gmail.com>
2026-01-08 13:52:27 +0700

    Initial Commit

---
+++ CShieldApp/CShieldAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CShieldAppApp.swift
+//  CShieldApp
+//
+//  Created by ThanhLe on 8/1/26.
+//
+
+import SwiftUI
+
+@main
+struct CShieldAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
